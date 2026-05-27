98a246c30216d9ee5ccfd23d5c2a6bbff8e1e9d8
Pablocheee <ordoaxio@gmail.com>
2026-05-27 23:42:38 +0400

    Update admin-chat.js

--- api/admin-chat.js
+++ api/admin-chat.js
@@ -41,7 +41,7 @@ export default async function handler(req, res) {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                model: 'llama3-70b-8192', // Используем быструю и точную модель
+                model: 'llama-3.3-70b-versatile', // Используем быструю и точную модель
                 messages: [
                     { role: 'system', content: systemPrompt },
                     { role: 'user', content: message }
